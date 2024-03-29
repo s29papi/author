d84870722d69569c34cc89d023e5b728eb2b3f4d
S29 <nots29vv@gmail.com>
2024-03-29 11:12:21 +0100

    first commit

---
+++ app/components/Box.tsx
@@ -0,0 +1,41 @@
+export default function Box() {
+    return (
+        <div className="mx-auto w-full p-0 border-collapse m-0">
+            <div className="mx-auto w-full grid rows-auto md:grid-cols-3 lg:grid-cols-3 px-8 justify-center max-w-9xl max-h-max lg:px-24 py-0 lg:py-16 gap-9">
+            <a href={"https://www.bountycaster.xyz/bounty/0xe0977d1c15231e6d6675741f40d17f141df8e5d0?widget=fc-2024-02-21"} title={"50,000 $degen bounty by @jrf | Bountycaster"} target="_blank" rel="noopener noreferrer">
+                <div className="items-center justify-center flex flex-col w-full mx-auto" style={{backgroundColor: '#131115', borderColor: '#3A393C'}}>
+                            <img className="w-full aspect-square object-contain" src={"https://www.bountycaster.xyz/bounty/0xe0977d1c15231e6d6675741f40d17f141df8e5d0/thumbs/frame.png?time=1711674712"} alt={"50,000 $degen bounty by @jrf | Bountycaster"} />
+                            <div className="p-4">
+                            <p className="text-white font-medium group-hover:text-accent-500 text-lg">
+                                {"50,000 $degen bounty by @jrf | Bountycaster"}
+                            </p>
+                            </div>
+                        
+                </div>
+            </a>
+            <a href={"https://www.bountycaster.xyz/bounty/0xe0977d1c15231e6d6675741f40d17f141df8e5d0?widget=fc-2024-02-21"} title={"50,000 $degen bounty by @jrf | Bountycaster"} target="_blank" rel="noopener noreferrer">
+                <div className="items-center justify-center flex flex-col w-full mx-auto" style={{backgroundColor: '#131115', borderColor: '#3A393C'}}>
+                            <img className="w-full aspect-square object-contain" src={"https://www.bountycaster.xyz/bounty/0xe0977d1c15231e6d6675741f40d17f141df8e5d0/thumbs/frame.png?time=1711674712"} alt={"50,000 $degen bounty by @jrf | Bountycaster"} />
+                            <div className="p-4">
+                            <p className="text-white font-medium group-hover:text-accent-500 text-lg">
+                                {"50,000 $degen bounty by @jrf | Bountycaster"}
+                            </p>
+                            </div>
+                        
+                </div>
+            </a>
+            <a href={"https://www.bountycaster.xyz/bounty/0xe0977d1c15231e6d6675741f40d17f141df8e5d0?widget=fc-2024-02-21"} title={"50,000 $degen bounty by @jrf | Bountycaster"} target="_blank" rel="noopener noreferrer">
+                <div className="items-center justify-center flex flex-col w-full mx-auto" style={{backgroundColor: '#131115', borderColor: '#3A393C'}}>
+                            <img className="w-full aspect-square object-contain" src={"https://www.bountycaster.xyz/bounty/0xe0977d1c15231e6d6675741f40d17f141df8e5d0/thumbs/frame.png?time=1711674712"} alt={"50,000 $degen bounty by @jrf | Bountycaster"} />
+                            <div className="p-4">
+                            <p className="text-white font-medium group-hover:text-accent-500 text-lg">
+                                {"50,000 $degen bounty by @jrf | Bountycaster"}
+                            </p>
+                            </div>
+                        
+                </div>
+            </a>
+            </div>
+        </div>
+    )
+}
